72639c1281f2b0d74538fa8037b97debcd6c19bc
Jamie Jennings <jamiejennings@users.noreply.github.com>
2018-04-22 12:16:18 -0400

    Added zip23 and map23

--- python/adapt23.py
+++ python/adapt23.py
@@ -16,8 +16,14 @@ try:
     HAS_UNICODE_TYPE = type(unicode) and True
     str23 = lambda s: str(s)
     bytes23 = lambda s: bytes(s)
+    zip23 = zip
+    map23 = map
 except NameError:
     HAS_UNICODE_TYPE = False
     str23 = lambda s: str(s, encoding='UTF-8')
     bytes23 = lambda s: bytes(s, encoding='UTF-8')
-
+    def zip23(*args):
+        return list(zip(*args))
+    def map23(fn, *args):
+        return list(map(fn, *args))
+    
