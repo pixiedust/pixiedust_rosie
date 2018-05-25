acd11170e2fae5444f37e9e928d4678917aaaef9
David Taieb <david_taieb@us.ibm.com>
2018-05-25 16:51:45 -0400

    Updated setup.py and added manifest.in to upload to Pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 setup(name='pixiedust_rosie',
-      version='0.1',
+      version='0.3',
       description='Data Wrangling',
       url='https://github.com/ibm-watson-data-lab/pixiedust_rosie',
       install_requires=['pixiedust', 'rosie'],
       author='Jamie Jenning, Terry Antony, David Taieb, Raj Singh',
-      author_email='',
+      author_email='info@rosie-lang.org,terryantony122@gmail.com,david_taieb@us.ibm.com',
       license='Apache 2.0',
       packages=find_packages(),
-      include_package_data=False,
+      include_package_data=True,
       zip_safe=False
      )
